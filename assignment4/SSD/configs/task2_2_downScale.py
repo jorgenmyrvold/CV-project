569ed68c8ac3225cc8cf1990fa82f3dbd6824fbb
IvarTG <ivartg@stud.ntnu.no>
2022-03-31 16:36:53 +0200

    compression trans

---
+++ assignment4/SSD/configs/task2_2_downScale.py
@@ -0,0 +1,40 @@
+from tops.config import LazyCall as L
+import torchvision
+from ssd.data.transforms import (
+    ToTensor, Normalize, Resize,
+    GroundTruthBoxesToAnchors, RandomHorizontalFlip, RandomSampleCrop)
+from albumentations import Downscale, JpegCompression, ImageCompression
+from .task2_1 import (
+    train,
+    optimizer,
+    anchors,
+    schedulers,
+    loss_objective,
+    model,
+    backbone,
+    data_train,
+    data_val,
+    # train_cpu_transform,
+    # val_cpu_transform,
+    # gpu_transform,
+    label_map
+)
+
+train_cpu_transform = L(torchvision.transforms.Compose)(transforms=[
+    L(RandomSampleCrop)(),
+    L(ToTensor)(),
+    L(RandomHorizontalFlip)(),
+    L(Downscale),
+    L(JpegCompression),
+    L(ImageCompression),
+    L(Resize)(imshape="${train.imshape}"),
+    L(GroundTruthBoxesToAnchors)(anchors="${anchors}", iou_threshold=0.5),
+])
+val_cpu_transform = L(torchvision.transforms.Compose)(transforms=[
+    L(ToTensor)(),
+    L(Resize)(imshape="${train.imshape}"),
+])
+gpu_transform = L(torchvision.transforms.Compose)(transforms=[
+    L(Normalize)(mean=[0.4765, 0.4774, 0.2259], std=[0.2951, 0.2864, 0.2878]),
+    L(RandomHorizontalFlip)(),
+])
