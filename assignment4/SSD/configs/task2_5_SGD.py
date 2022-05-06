c09fbd706f12e5305e82926f52bf69f72bf080f0
Jørgen Myrvold <jorgeum@stud.ntnu.no>
2022-05-06 14:39:04 +0200

    Added videos and config files for task 2.5

---
+++ assignment4/SSD/configs/task2_5_SGD.py
@@ -0,0 +1,25 @@
+from tops.config import LazyCall as L
+from .utils import get_dataset_dir
+
+from .task2_3_w_init import (
+    train,
+    optimizer,
+    anchors,
+    schedulers,
+    loss_objective,
+    model,
+    backbone,
+    data_train,
+    data_val,
+    anchors,
+    train_cpu_transform,
+    val_cpu_transform,
+    gpu_transform,
+    label_map
+)
+
+data_train.dataset.img_folder = get_dataset_dir("tdt4265_2022_updated")
+data_train.dataset.annotation_file = get_dataset_dir("tdt4265_2022_updated/train_annotations.json")
+data_val.dataset.img_folder = get_dataset_dir("tdt4265_2022_updated")
+data_val.dataset.annotation_file = get_dataset_dir("tdt4265_2022_updated/val_annotations.json")
+
